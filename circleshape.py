a8b485d4a4aa5b72af0d17c6c0bece55b4fd7e8a
Musingmusician <musingmusician@gmail.com>
2025-11-02 13:46:22 -0500

    We have shooting and death.  What else would we want?

--- circleshape.py
+++ circleshape.py
@@ -20,3 +20,6 @@ class CircleShape(pygame.sprite.Sprite):
     def update(self, dt):
         # sub-classes must override
         pass
+
+    def collide(self, asteroid):
+        return self.radius + asteroid.radius >= pygame.Vector2.distance_to(self.position,asteroid.position)
--- main.py
+++ main.py
@@ -2,11 +2,13 @@
 # the open-source pygame library
 # throughout this file
 import pygame
+import sys
 from constants import *
 from circleshape import *
 from player import *
 from asteroid import *
 from asteroidfield import *
+from shot import *
 
 def main():
     print("Starting Asteroids!")
@@ -17,9 +19,11 @@ def main():
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
     asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
     AsteroidField.containers = (updatable)
     Asteroid.containers = (updatable, drawable, asteroids)
     Player.containers = (updatable, drawable)
+    Shot.containers = (updatable,drawable, shots)
     main_player = Player(SCREEN_WIDTH/2,SCREEN_HEIGHT/2)
     field = AsteroidField()
     clock = pygame.time.Clock()
@@ -30,6 +34,13 @@ def main():
                 return
         updatable.update(dt)
         screen.fill((0,0,0))
+        for asteroid in asteroids:
+            for shot in shots:
+                if shot.collide(asteroid):
+                    asteroid.kill()
+        for asteroid in asteroids:
+            if main_player.collide(asteroid):
+                sys.exit("Game over!")
         for item in drawable:
             item.draw(screen)
         pygame.display.flip()
