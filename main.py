20eb92444aafaafc21824ac83cd388685098a452
Musingmusician <musingmusician@gmail.com>
2025-11-01 21:41:48 -0400

    We have a triangle player

--- main.py
+++ main.py
@@ -3,6 +3,8 @@
 # throughout this file
 import pygame
 from constants import *
+from circleshape import *
+from player import *
 
 def main():
     print("Starting Asteroids!")
@@ -10,13 +12,18 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    ## screen_surface = pygame.surface((SCREEN_WIDTH,SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        main_player = Player(SCREEN_WIDTH/2,SCREEN_HEIGHT/2)
         screen.fill((0,0,0))
+        main_player.draw(screen)
         pygame.display.flip()
+        clock.tick(60)
+        dt += clock.tick()/1000
 
 
 if __name__ == "__main__":
