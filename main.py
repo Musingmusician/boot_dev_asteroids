61f46dec198404efcc26dc7a63d9277613244c5b
Musingmusician <musingmusician@gmail.com>
2025-11-01 22:03:37 -0400

    left and right turning added? I'm running with it.

--- main.py
+++ main.py
@@ -20,10 +20,10 @@ def main():
                 return
         main_player = Player(SCREEN_WIDTH/2,SCREEN_HEIGHT/2)
         screen.fill((0,0,0))
+        main_player.update(dt)
         main_player.draw(screen)
         pygame.display.flip()
-        clock.tick(60)
-        dt += clock.tick()/1000
+        dt = clock.tick(60)/1000
 
 
 if __name__ == "__main__":
